9466b9c0ab070cb6c9ec7a30c1009303d55e1822
rshsm <arashsami@live.com>
2023-10-06 21:05:03 +0100

    remove raw response

--- main.go
+++ main.go
@@ -10,35 +10,35 @@ import (
 	"bufio"
 )
 
-// Define a struct to hold a single message
 type Message struct {
 	Role    string `json:"role"`
 	Content string `json:"content"`
 }
 
-// Define a struct to hold the API request payload
 type Payload struct {
 	Model   string    `json:"model"`
 	Messages []Message `json:"messages"`
 }
 
-func main() {
-	// Create a new HTTP client
-	client := &http.Client{}
+type Response struct {
+	Choices []struct {
+		Text string `json:"text"`
+	} `json:"choices"`
+}
 
-	// Set up a loop to continuously prompt the user for input
+func main() {
+	client := &http.Client{}
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("You: ")
-		scanner.Scan()  // Read user input from CLI
+		scanner.Scan()
 		userMessage := scanner.Text()
 
-		// Exit loop if user types "exit"
 		if userMessage == "exit" {
 			break
 		}
 
-		// Prepare the API request payload
 		payload := Payload{
 			Model: "gpt-3.5-turbo",
 			Messages: []Message{
@@ -47,25 +47,21 @@ func main() {
 			},
 		}
 
-		// Marshal the payload to JSON
 		payloadBytes, err := json.Marshal(payload)
 		if err != nil {
 			fmt.Println("Error marshalling payload:", err)
 			return
 		}
 
-		// Create a new HTTP request
-		req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(payloadBytes))
+		req, err := http.NewRequest("POST", "https://api.openai.com/v1/engines/davinci-codex/completions", bytes.NewBuffer(payloadBytes))
 		if err != nil {
 			fmt.Println("Error creating request:", err)
 			return
 		}
 
-		// Set the necessary headers
 		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Authorization", "Bearer YOUR_API_KEY")
 
-		// Send the request and get the response
 		resp, err := client.Do(req)
 		if err != nil {
 			fmt.Println("Error sending request:", err)
@@ -73,12 +69,23 @@ func main() {
 		}
 		defer resp.Body.Close()
 
-		// Read and print the raw API response
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Println("Error reading response:", err)
 			return
 		}
-		fmt.Println("API Response:", string(body))
+
+		var response Response
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			fmt.Println("Error unmarshalling response:", err)
+			return
+		}
+
+		if len(response.Choices) > 0 {
+			fmt.Println("ChatGPT:", response.Choices[0].Text)
+		} else {
+			fmt.Println("No response received")
+		}
 	}
 }
